5c16e3e923d478defcf49e7e177093beb2b0b176
Nahim <nahimmartinez03@gmail.com>
2026-04-03 12:21:01 -0300

    feat: Creé los modelos para la aplicación usuarios

--- Contenedor/usuarios/models.py
+++ Contenedor/usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoUsuario(models.Model):
+    nombre = models.CharField(max_length=25)
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    dni = models.CharField(max_length=100, unique=True)
+    tipo_usuario = models.ForeignKey(TipoUsuario, on_delete=models.SET_NULL, null=True)
