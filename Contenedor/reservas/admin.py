96192b4e09249989f5b24936dc5cfcb58d454b8f
Nahim <nahimmartinez03@gmail.com>
2026-04-03 17:43:42 -0300

    feat: Agregué los modelos para poder utilizarlos en el admin site

--- Contenedor/reservas/admin.py
+++ Contenedor/reservas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import EstadoReserva, MetodoPago, Reserva, Pago
 # Register your models here.
+
+admin.site.register(EstadoReserva)
+admin.site.register(MetodoPago)
+admin.site.register(Reserva)
+admin.site.register(Pago)
--- Contenedor/vehiculos/admin.py
+++ Contenedor/vehiculos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import EstadoVehiculo, TipoVehiculo, Marca, Modelo, Vehiculo
 # Register your models here.
+
+admin.site.register(EstadoVehiculo)
+admin.site.register(TipoVehiculo)
+admin.site.register(Marca)
+admin.site.register(Modelo)
+admin.site.register(Vehiculo)
